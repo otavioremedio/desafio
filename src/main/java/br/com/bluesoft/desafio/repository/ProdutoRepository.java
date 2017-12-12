cf185f741221690ed3ae77288554492a9361f921
otavioremedio <otavioremedio@gmail.com>
2017-12-12 20:59:11 -0200

    .

--- src/main/java/br/com/bluesoft/desafio/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.bluesoft.desafio.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.bluesoft.desafio.model.Produto;
-
-public interface ProdutoRepository extends CrudRepository<Produto, String> {
-}
